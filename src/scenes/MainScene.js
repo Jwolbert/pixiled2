1c79d51e214fa399a139bce19670db8bbe077c19
Jwolbert <jtwolbertdev@gmail.com>
2022-06-30 21:12:15 -0400

    git ignore

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,5 +1,4 @@
 import Phaser from "phaser";
-import NpcMap from "../entity/npc/NpcMap";
 import Player from "../entity/player/Player";
 import AnimationUtility from "../utility/AnimationUtility";
 import Attack from "../entity/action/attack/Attack";
@@ -8,12 +7,14 @@ import GameWebSocket from "../websocket/GameWebSocket";
 export class Example extends Phaser.Scene
 {
     entities = {};
+    data = {};
     player;
     websocket;
 
-    constructor ()
+    constructor (websocket)
     {
         super();
+        this.websocket = websocket;
     }
 
     preload ()
@@ -27,63 +28,29 @@ export class Example extends Phaser.Scene
 
     create ()
     {
-
         console.log("STARTING_SCENE");
-
         AnimationUtility.call(this, ['hatman', 'slash']);
         const map = this.make.tilemap({ key: 'map' });
-        // const npcMap = new NpcMap(map);
-
-        // The first parameter is the name of the tileset in Tiled and the second parameter is the key
-        // of the tileset image used when loading the file in preload.
         const tiles = map.addTilesetImage('jawbreaker_tiles', 'ruins', 32, 32, 1, 2);
-
-        // You can load a layer from the map using the layer name from Tiled, or by using the layer
-        // index (0 in this case).
         const layer = map.createLayer(0, tiles, 0, 0);
-
         map.setCollision([ 2, 18, 26, 34, 35, 41, 42, 36, 37, 28, 20, 21, 22, 30, 29, 46 ]);
-
         layer.width = 200;
-
         this.character = this.physics.add.sprite(48, 48, 'mainCharacters').setScale(.9).setDepth(3);
-        // this.npcCharacter = this.physics.add.sprite(500, 250, 'mainCharacters').setDepth(3);
-
         this.physics.add.collider(this.character, layer);
-        // this.physics.add.collider(this.npcCharacter, layer);
-
         this.cameras.main.setZoom(1.5);
         this.cameras.main.startFollow(this.character);
-
         this.player = new Player('hatman', this.character, this.input);
         this.entities[this.player.getId()] = this.player;
-
-        // const npc = new Npc('hatman', this.npcCharacter);
-        // this.entities[npc.getId()] = npc;
-
-        // npcMap.dStar(player, npc);
-
-        /*
-        setTimeout(() => {
-            player.dead = true;
-        }, 60000);
-        */
-       setTimeout(() => {
-        console.log(this.count);
-       },1000);
-
-       this.websocket = new GameWebSocket(this.entities, this.player, this.physics); 
+        this.websocket = new GameWebSocket(this.entities, this.player, this.physics); 
     }
 
 
     update ()
     {
-        Object.keys(this.entities).forEach((id) => {
-            this.entities[id].update();
+        Object.values(this.entities).forEach((entity) => {
+            entity.update();
         });
-
         this.bringOutYourDead();
-
         this.websocket.update();
         this.performActions();
     }
